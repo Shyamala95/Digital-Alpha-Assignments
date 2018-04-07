33870084ffa8cfb02d31ce3f13f5a0bc3f314094
Shyamala95 <35482456+Shyamala95@users.noreply.github.com>
2018-04-07 17:32:52 +0530

    Add files via upload

---
+++ apr_5 sentences.py
@@ -0,0 +1,12 @@
+string=input("enter the sentences")
+c1=0
+c2=0
+
+for i in string:
+      if(i.isdigit()):
+            c1=c1+1
+      c2=c2+1
+print("The number of digits is:")
+print(c1)
+print("The number of letters is:")
+print(c2)
