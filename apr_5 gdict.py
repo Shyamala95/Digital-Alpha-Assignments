cb6c032bc08629219742cf53f8f4dcb3355d69df
Shyamala95 <35482456+Shyamala95@users.noreply.github.com>
2018-04-08 23:59:18 +0530

    Add files via upload

---
+++ apr_5 gdict.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 23:51:37 2018
+
+@author: Shyamala"""
+d={}
+n=int(input("Please enter value of n: "))
+for i in range(1,n+1):
+    d[i]=i**i
+print(d)
